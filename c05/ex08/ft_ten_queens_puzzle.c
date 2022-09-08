85aea65af5a8247d38a500a1ed3c3a449cedfb6d
jyjcoder <100260069+jyjcoder@users.noreply.github.com>
2022-09-08 12:40:41 +0900

    Update ft_ten_queens_puzzle.c

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -11,3 +11,76 @@
 /* ************************************************************************** */
 
 int	ft_ten_queens_puzzle(void);
+
+#include <unistd.h>
+
+int	ft_abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	return (a);
+}
+
+
+void print_queen(){
+    int i;
+    char q;
+    i = 0;
+
+    while(i < 10){
+        q = arr[i] + '0';
+        write(1, 'i', 1);
+        i++;
+    } 
+    write(1, "\n", 1);
+}
+
+void check(){
+    int j;
+
+    j = 0;
+    while(j < col){
+        if(arr[j] == arr[col])
+            return(1);
+        if(ft_abs(j-col) == ft_abs(arr[j]- arr[col]))
+            return(1);
+        j++;
+    }
+    return(0);
+}
+
+void	dfs(int col, int *arr, int *answer)
+{
+	int	i;
+
+	i = 0;
+	if (col == 10)
+	{
+		print_chess(arr);
+		*answer = *answer + 1;
+		return ;
+	}
+	else
+	{
+		while (i < 10)
+		{
+			arr[col] = i;
+			if (check(col, arr) == 0)
+				dfs(col + 1, arr, answer);
+			i++;
+		}
+	}
+}
+
+int main(){
+    int arr[10];
+    int i;
+    int count;
+    int *answer;
+
+    i = 0;
+    answer = &count;
+    count = 0;
+    dfs(0, arr, answer);
+    return(*answer);
+}
